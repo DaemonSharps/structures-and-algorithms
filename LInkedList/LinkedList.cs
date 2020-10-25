56dd975f2024232ec267e2ce5507a6b6e55c7278
Денис Смирнов <66mrpechenka66@gmail.com>
2020-10-25 15:55:43 +0300

    Start writштп linked list

---
+++ LInkedList/LinkedList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace structures_and_algorithms.LInkedList
+{
+    class LinkedList<T> : IEnumerable<T>
+    {
+        Node<T> head;
+        Node<T> tail;
+        int count;
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (head==null)
+            {
+                head = node;
+            }
+            else
+            {
+                tail.Next = node;
+            }
+            tail = node;
+            count++;
+        }
+        public bool Remove(T data)
+        {
+            Node<T> current = head;
+            Node<T> previous = null;
+            while (current!=null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    if (previous!=null)
+                    {
+                        previous.Next = current.Next;
+                        if (current.Next == null)
+                            tail = previous;
+                    }
+                    else
+                    {
+                        head = head.Next;
+                        if (head == null)
+                            tail = null;
+                    }
+                    count--;
+                    return true;
+                }
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LInkedList/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace structures_and_algorithms
+{
+    class Node<T>
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public Node<T> Next {get;set;}
+    }
+}
