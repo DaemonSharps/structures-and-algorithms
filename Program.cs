2f4744cff61e05c4e560a0d5b208de6c6f6f0628
Денис Смирнов <66mrpechenka66@gmail.com>
2020-10-25 15:01:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace structures_and_algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
