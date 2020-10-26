34f27c61d4e54e3374a3ea429037cbd66471a7e3
Денис Смирнов <66mrpechenka66@gmail.com>
2020-10-26 22:41:04 +0300

    Add Fast Pow

---
+++ Algorithms/Fast Pow/Pow.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace structures_and_algorithms.Algorithms.Fast_Pow
+{
+    public static class Pow
+    {
+        public static double Recursive(double number,int degree)
+        {
+            if (degree == 0)
+                return 1;
+            if (degree%2==0)
+            {
+                var p = Recursive(number, degree / 2);
+                return p * p;
+            }
+            else
+            {
+                return number * Recursive(number,degree-1);
+            }
+        }
+        public static double Byte(double number, int degree)
+        {
+            if (degree == 0)
+                return 1;
+            if ((degree&1)==0)// последний бит равен 0 - четное число
+            {
+                var p = Byte(number,degree>>1);//смещение на один бит как деление на два
+                return p * p;
+            }
+            else
+            {
+                return number * Byte(number,degree-1);
+            }
+        }
+        public static double Iter(double number, int degree)
+        {
+            var res = 1D;
+            while (degree>0)
+            {
+                if ((degree&1)==0)
+                {
+                    number *= number;
+                    degree >>= 1;
+                }
+                else
+                {
+                    res *= number;
+                    --degree;
+
+                }
+            }
+            return res;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using structures_and_algorithms.LInkedList;
+﻿using structures_and_algorithms.Algorithms.Fast_Pow;
+using structures_and_algorithms.LInkedList;
+using structures_and_algorithms.Algorithms;
 using System;
 using System.Security.Cryptography.X509Certificates;
 
@@ -19,7 +21,14 @@ namespace structures_and_algorithms
                 Console.WriteLine(LList.contains("dashulya"));
                 Console.WriteLine(LList.contains("Dens"));
             }
-            
+            {
+                for (int i=0;i<5;i++)
+                {
+                    Console.WriteLine($"{i} in {i*2} ={Pow.Recursive(i, i * 2)}");
+                    Console.WriteLine($"{i} in {i + 2} ={Pow.Iter(i, i * 2)}");
+                    Console.WriteLine($"{i} in {15} ={Pow.Byte(i, 15)}");
+                }
+            }
             Console.ReadLine();
         }
     }
