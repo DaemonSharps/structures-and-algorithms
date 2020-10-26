8e6386be52b72d35dae1823aaba5fa4f463252a7
Денис Смирнов <66mrpechenka66@gmail.com>
2020-10-26 23:24:27 +0300

    override func Mnimum

--- Algorithms/Minimum.cs
+++ Algorithms/Minimum.cs
@@ -6,6 +6,8 @@ namespace structures_and_algorithms.Algorithms
 {
  public static   class Minimum
     {
+        public static int Find(int a, int b) => a < b ? a : b;
         public static int Find(int a,int b,int c)=>(a=a<b?a:b)<c?a:c;
+        
     }
 }
