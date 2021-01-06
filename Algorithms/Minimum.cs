6c51b17baf69375a3fa6e40789683a196096b52b
Денис Смирнов <smirnov.god@gmail.com>
2021-01-06 22:53:27 +0300

    dd

--- Algorithms/Minimum.cs
+++ Algorithms/Minimum.cs
@@ -8,7 +8,7 @@ namespace structures_and_algorithms.Algorithms
     {
         public static int Find(int a, int b) => a < b ? a : b;
         public static int Find(int a,int b,int c)=>(a=a<b?a:b)<c?a:c;
-        public static void finnssa() { }
+        public static void finnaaaassa() { }
         
     }
 }
